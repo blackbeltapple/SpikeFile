d50862626d897e17e0564491d4dd32f7c7beda22
amanda manekshaw <amanda_manekshaw@hotmail.com>
2017-01-10 15:27:21 +0000

    [SPIKE-4] Use multer module to receive file via HTTP request from postman. Req.files undefined

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var fileUpload = require('express-fileupload');
 var s3 = require('s3');
+var multer = require('multer');
+var upload = multer({dest: './forupload/'})
 var client = require('./s3init.js');
-
 
 const app = express();
 
-app.use(bodyParser.json())
+// app.use(fileUpload());
+app.use(bodyParser.json());
 
 app.get('/', function (req, res, next) {
   res.send('Hello World - Spike File');
@@ -16,10 +19,23 @@ app.get('/demo', function (req, res, next) {
   res.send('Watch the demo!');
 });
 
-app.get('/upload', function (req, res, next) {
+// This does upload the file from the HTTP request and puts it in the 'dest' folder
+// It gives it a hashed name, but req.files is still undefined
+app.post('/multer', upload.single('filename'), function (req, res, next) {
+  console.log('req.files ', req.files);
+});
+
+// Couldn't get this to work - express-fileupload - low # downloads so ditching this module
+// app.post('/uploadfromrequest', function (req, res, next) {
+//   console.log('HELLO')
+//   console.log('req.files ', req.files);
+//   // res.send('Watch the demo!');
+// });
+
+// S3 upload of file from local folder to S3 bucket worked fine.
+app.get('/uploadlocal', function (req, res, next) {
   var params = {
   localFile: "forupload/benedict.jpg",
-
   s3Params: {
     Bucket: "blackbeltapple",
     Key: "benedict2.jpg",
