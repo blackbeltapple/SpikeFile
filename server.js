b865e04d1b5b6cd6d7788bf00d13124caf34c3b2
amanda manekshaw <amanda_manekshaw@hotmail.com>
2017-01-10 14:46:48 +0000

    [SPIKE-4] Re-organise S3 initialisation into separate file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var s3 = require('s3');
-var secret = require('./secret.js');
+var client = require('./s3init.js');
+
 
 const app = express();
 
@@ -21,7 +22,7 @@ app.get('/upload', function (req, res, next) {
 
   s3Params: {
     Bucket: "blackbeltapple",
-    Key: "benedict.jpg",
+    Key: "benedict2.jpg",
     // other options supported by putObject, except Body and ContentLength.
     // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
   },
@@ -40,19 +41,6 @@ app.get('/upload', function (req, res, next) {
 // res.send('Upload route');
 });
 
-var client = s3.createClient({
-  maxAsyncS3: 20,     // this is the default
-  s3RetryCount: 3,    // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    accessKeyId: secret.key,
-    secretAccessKey: secret.secret,
-    // any other options are passed to new AWS.S3()
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-  },
-});
 
 
 
