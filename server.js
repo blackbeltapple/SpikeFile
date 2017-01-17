13cccb34b327b48d5dabc16385f81aa4292b4e4b
amanda manekshaw <amanda_manekshaw@hotmail.com>
2017-01-17 17:42:29 +0000

    Add some extra comments that were useful when revisited this spike

--- server.js
+++ server.js
@@ -18,16 +18,30 @@ app.get('/', function (req, res, next) {
 });
 
 // Node module: MULTER
-// This does upload the file from the HTTP request and puts it in the 'dest' folder
-// It gives it a hashed name. req.file gives us file details
-// Sent test request using postman - attached file in boy with key 'filename', no headers set explicitly
+// This route tests multer
+// This does upload the file from the HTTP request and puts it in the 'dest' folder on local machine (/for upload)
+// It gives it a hashed name.
+// Sent test request using postman:  attach file in body (in 'form-data' mode) with key 'filename'. No headers set explicitly
 // If not working correctly, try a new tab in postman with fresh request
+// req.file returns us the following file details:
+// {
+//   "fieldname": "filename",
+//   "originalname": "egghead-react-cheat-sheet-0-14-7.pdf",
+//   "encoding": "7bit",
+//   "mimetype": "application/pdf",
+//   "destination": "./forupload/",
+//   "filename": "c328ee52c1607a82d52d27e819a97ecb",
+//   "path": "forupload/c328ee52c1607a82d52d27e819a97ecb",
+//   "size": 265151
+// }
 app.post('/multer', upload.single('filename'), function (req, res, next) {
   console.log('req.file ', req.file);
+  res.send({reqfile: req.file})
 });
 
+
 // Node module: express-fileupload
-// Low # downloads, so ditching this module
+// Low # downloads, so not going to use this module
 //
 // app.post('/uploadfromrequest', function (req, res, next) {
 //   console.log('HELLO')
@@ -35,16 +49,17 @@ app.post('/multer', upload.single('filename'), function (req, res, next) {
 //   // res.send('Watch the demo!');
 // });
 
+
 // S3 upload of file from local folder to S3 bucket worked fine.
 app.get('/uploadlocal', function (req, res, next) {
   var params = {
-  localFile: "forupload/benedict.jpg",
-  s3Params: {
-    Bucket: "blackbeltapple",
-    Key: "benedict2.jpg",
-    // other options supported by putObject, except Body and ContentLength.
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-  },
+    localFile: "forupload/benedict.jpg",
+    s3Params: {
+      Bucket: "student-portal",
+      Key: "benedict7.jpg",
+      // other options supported by putObject, except Body and ContentLength.
+      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+    },
   };
   var uploader = client.uploadFile(params);
   uploader.on('error', function(err) {
